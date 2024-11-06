ecef0629b68e3d5a600efc8efd5a1ea1b412c5f1
singhkunal2050 <singhkunal2050@gmail.com>
2024-11-06 18:34:28 +0530

    Added Config for Static and PWA Apps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), // Static HTML page
+        spa: resolve(__dirname, "spa.html"), // Entry point for SPA
+      },
+    },
+  },
+});
